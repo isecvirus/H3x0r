bb22eb389919ddacbdfcbc21bcc51876cae755f0
Virus <43806178+isecvirus@users.noreply.github.com>
2022-09-11 03:00:26 +0300

    Patch
    Sha224 hash on sha1

--- hSha224.py
+++ hSha224.py
@@ -3,6 +3,6 @@ import hashlib
 
 def Sha224(string: bytes, type: str):
     if type == 'hexdigest':
-        return hashlib.sha1(string).hexdigest()
+        return hashlib.sha224(string).hexdigest()
     elif type == 'digest':
-        return str(hashlib.sha1(string).digest())[2:-1]
+        return str(hashlib.sha224(string).digest())[2:-1]
