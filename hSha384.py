5c95ed297e51d9211a95d727f399a851d18242b8
Virus <43806178+isecvirus@users.noreply.github.com>
2022-11-17 18:41:05 +0300

    Delete hSha384.py

--- hSha384.py
+++
@@ -1,8 +0,0 @@
-import hashlib
-
-
-def Sha384(string: bytes, type: str):
-    if type == 'hexdigest':
-        return hashlib.sha384(string).hexdigest()
-    elif type == 'digest':
-        return str(hashlib.sha384(string).digest())[2:-1]
