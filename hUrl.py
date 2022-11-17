ffdbf599fe2dd6daf9aa58fa8aa4a593aec07b58
Virus <43806178+isecvirus@users.noreply.github.com>
2022-11-17 18:37:56 +0300

    Delete hUrl.py

--- hUrl.py
+++
@@ -1,9 +0,0 @@
-import urllib.parse
-
-class URL:
-    def encode(self, url):
-        return urllib.parse.quote(url)
-    def decode(self, url):
-        return urllib.parse.unquote(url)
-
-Url = URL()
