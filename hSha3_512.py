d4b9a5d12946162e23aad932fcf46b82f4ec86e4
Virus <43806178+isecvirus@users.noreply.github.com>
2022-11-17 18:41:22 +0300

    Delete hSha3_512.py

--- hSha3_512.py
+++
@@ -1,8 +0,0 @@
-import hashlib
-
-
-def Sha3_512(string: bytes, type: str):
-    if type == 'hexdigest':
-        return hashlib.sha3_512(string).hexdigest()
-    elif type == 'digest':
-        return str(hashlib.sha3_512(string).digest())[2:-1]
