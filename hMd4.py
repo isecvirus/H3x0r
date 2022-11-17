2bff2b7598004ff42d81f2ba3f37d8e157dae38c
Virus <43806178+isecvirus@users.noreply.github.com>
2022-11-17 18:38:08 +0300

    Delete hMd4.py

--- hMd4.py
+++
@@ -1,8 +0,0 @@
-from Crypto.Hash import MD4
-
-
-def _Md4_(string:bytes, type:str):
-    if type == "hexdigest":
-        return MD4.new(string).hexdigest()
-    elif type == "digest":
-        return MD4.new(string).digest()
