a4c3ebc1b0f596aefcfadece759b7f12d8768bad
Virus <43806178+isecvirus@users.noreply.github.com>
2022-11-17 18:39:11 +0300

    Delete hRipemd160.py

--- hRipemd160.py
+++
@@ -1,8 +0,0 @@
-import hashlib
-
-
-def Ripemd_160(string:bytes, type:str):
-    if type == "hexdigest":
-        return hashlib.new('ripemd160', string).hexdigest()
-    elif type == "digest":
-        return str(hashlib.new('ripemd160', string).digest())[2:-1]
