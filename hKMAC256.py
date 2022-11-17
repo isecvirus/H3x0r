e22844a7e7aa4d9cd0c73b660f1b1ea4b4ee4d81
Virus <43806178+isecvirus@users.noreply.github.com>
2022-11-17 18:37:05 +0300

    Delete hKMAC256.py

--- hKMAC256.py
+++
@@ -1,7 +0,0 @@
-from Crypto.Hash import KMAC256
-
-def _KMAC256_(data:str, key:str, type:str):
-    if type == "hexdigest":
-        return KMAC256.new(data=data.encode(), key=key.encode()).hexdigest()
-    elif type == "digest":
-        return KMAC256.new(data=data.encode(), key=key.encode()).digest()
