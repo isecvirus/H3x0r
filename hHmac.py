4ef12fc836f143b3f51ea510b1da1a4e7d4335ca
Virus <43806178+isecvirus@users.noreply.github.com>
2022-11-17 18:36:52 +0300

    Delete hHmac.py

--- hHmac.py
+++
@@ -1,8 +0,0 @@
-import hmac
-
-
-def Hmac(key: str, msg: str, digestmod: str, type: str):
-    if type == 'hexdigest':
-        return hmac.new(key=key.encode(), msg=msg.encode(), digestmod=digestmod).hexdigest()
-    elif type == 'digest':
-        return str(hmac.new(key=key.encode(), msg=msg.encode(), digestmod=digestmod).digest())[2:-1]
