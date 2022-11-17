4166c0d9e75cd22028df711a3a6f757be97f02ab
Virus <43806178+isecvirus@users.noreply.github.com>
2022-11-17 18:41:00 +0300

    Delete hSha256.py

--- hSha256.py
+++
@@ -1,8 +0,0 @@
-import hashlib
-
-
-def Sha256(string: bytes, type: str):
-    if type == 'hexdigest':
-        return hashlib.sha256(string).hexdigest()
-    elif type == 'digest':
-        return str(hashlib.sha256(string).digest())[2:-1]
