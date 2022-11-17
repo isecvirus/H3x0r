9512e9f2328bdc10c62aacb509fb1d2723275864
Virus <43806178+isecvirus@users.noreply.github.com>
2022-11-17 18:39:18 +0300

    Delete hSha1.py

--- hSha1.py
+++
@@ -1,8 +0,0 @@
-import hashlib
-
-
-def Sha1(string: bytes, type: str):
-    if type == 'hexdigest':
-        return hashlib.sha1(string).hexdigest()
-    elif type == 'digest':
-        return str(hashlib.sha1(string).digest())[2:-1]
