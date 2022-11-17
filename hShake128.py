d86f7d00836459ae3b56d4694b7d9bfe90d4fbf1
Virus <43806178+isecvirus@users.noreply.github.com>
2022-11-17 18:41:25 +0300

    Delete hShake128.py

--- hShake128.py
+++
@@ -1,8 +0,0 @@
-import hashlib
-
-
-def Shake128(string: bytes, type: str, length: int):
-    if type == 'hexdigest':
-        return hashlib.shake_128(string).hexdigest(length)
-    elif type == 'digest':
-        return str(hashlib.shake_128(string).digest(length))[2:-1]
